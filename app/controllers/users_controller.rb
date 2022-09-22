90fc67eadffc19502e26f88377d2880d8376ba13
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-09-22 14:49:39 +0000

    [Finish] meshiOK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
@@ -20,5 +20,5 @@ private
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-  
+
 end
